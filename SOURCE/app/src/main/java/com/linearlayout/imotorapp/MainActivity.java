38df0c62905dc64f89292ea342288091318befc7
Tran Minh Son <kakalala2003@gmail.com>
2019-07-20 16:07:09 +0700

    Bổ sung ProgressDialog cho Login

--- SOURCE/app/src/main/java/com/linearlayout/imotorapp/MainActivity.java
+++ SOURCE/app/src/main/java/com/linearlayout/imotorapp/MainActivity.java
@@ -2,9 +2,11 @@ package com.linearlayout.imotorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -30,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     Button btnLogin;
     EditText edtPhoneNumber;
     String phoneNumber;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,48 +42,69 @@ public class MainActivity extends AppCompatActivity {
         Xuly();
 
     }
-    void init()
-    {
+
+    void init() {
         btnLogin = findViewById(R.id.btn_login);
         edtPhoneNumber = findViewById(R.id.edt_phone_number);
     }
 
-    void Xuly()
-    {
+    void Xuly() {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+
+
                 getdata();
-                if(edtPhoneNumber.getText().toString().equals(phoneNumber))
-                {
 
-                    Intent intent =  new Intent(MainActivity.this,HomeActivity.class);
-                    setPhoneNumber();
-                    startActivity(intent);
-                }
-                else
 
-                {
+                final ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+                progressDialog.setTitle("LoginProcessing");
+                progressDialog.setMessage("Loading........");
+                progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                progressDialog.show();
 
-                    Toast.makeText(MainActivity.this, "Không dăng nhập thành công", Toast.LENGTH_SHORT).show();
-                }
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (edtPhoneNumber.getText().toString().equals(phoneNumber)) {
+
+                            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                            setPhoneNumber();
+
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            progressDialog.dismiss();
+
+                            Toast.makeText(MainActivity.this, "Vui lòng kiểm tra lại số điện thoại", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                },5000);
+
+
+
+
             }
         });
 
     }
+
     //Hàm lưu vào sharepreferences
-    void setPhoneNumber()
-    {
-        SharedPreferences sharedPreferences = getSharedPreferences("ImotorSource",MODE_PRIVATE);
+    void setPhoneNumber() {
+        SharedPreferences sharedPreferences = getSharedPreferences("ImotorSource", MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString("phoneNumber",phoneNumber);
+        editor.putString("phoneNumber", phoneNumber);
         editor.apply();
 
 
     }
-    void getdata()
-    {
-        GetLoginInfo getLoginInfo = new GetLoginInfo("0387490078","hjgjhg","android");
+
+    public void getdata() {
+
+
+        GetLoginInfo getLoginInfo = new GetLoginInfo("0387490078", "hjgjhg", "android");
         Retrofit retrofit = new Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create())
                 .baseUrl("http://45.118.144.19:1904/api/")
@@ -92,10 +116,9 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     String strJson = response.body().string();
                     Gson gson = new Gson();
-                    Login login = gson.fromJson(strJson,Login.class);
-                    phoneNumber=login.getResult().getPhone().toString();
-                    Log.d("Show phone Number",""+phoneNumber.toString());
-
+                    Login login = gson.fromJson(strJson, Login.class);
+                    phoneNumber = login.getResult().getPhone().toString();
+                    Log.d("Show phone Number", "" + phoneNumber.toString());
 
 
                 } catch (IOException e) {
@@ -112,9 +135,8 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    class GetLoginInfo
-    {
-        String phoneNumber,DeviceID,Os;
+    class GetLoginInfo {
+        String phoneNumber, DeviceID, Os;
 
         public GetLoginInfo(String phoneNumber, String deviceID, String os) {
             this.phoneNumber = phoneNumber;
